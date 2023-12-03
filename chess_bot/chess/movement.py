cdb93cee9153e591343fae88e187ec1a2d226845
Will Udstrand <will.udstrand@gmail.com>
2023-12-02 19:13:56 -0500

    Adding in scaffolding for movement

--- chess_bot/chess/movement.py
+++ chess_bot/chess/movement.py
@@ -4,6 +4,13 @@ from chess_bot.chess.piece import Piece
 from chess_bot.chess.board.position import Position
 
 
+# Should this code generate all move to n-depth or should it just do on turn worth....
+#
+# How to handle testing the moves legality?
+# Need
+#   - board
+#   - piece movement history (i.e. is the first move)
+
 def generate_moves(piece: Piece, depth: int = 1) -> List[Position]:
     pass
 
