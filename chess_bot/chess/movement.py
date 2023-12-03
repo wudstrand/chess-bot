574d5cbcb324f192d363379004e5946ffc5ae59b
Will Udstrand <will.udstrand@gmail.com>
2023-12-02 19:12:03 -0500

    Adding in scaffolding for movement

--- chess_bot/chess/movement.py
+++ chess_bot/chess/movement.py
@@ -6,3 +6,27 @@ from chess_bot.chess.board.position import Position
 
 def generate_moves(piece: Piece, depth: int = 1) -> List[Position]:
     pass
+
+
+def _pawn_move(position: Position, depth: int = 1) -> List[Position]:
+    pass
+
+
+def _knight_move(position: Position, depth: int = 1) -> List[Position]:
+    pass
+
+
+def _bishop_move(position: Position, depth: int = 1) -> List[Position]:
+    pass
+
+
+def _rook_move(position: Position, depth: int = 1) -> List[Position]:
+    pass
+
+
+def _queen_move(position: Position, depth: int = 1) -> List[Position]:
+    pass
+
+
+def _king_move(position: Position, depth: int = 1) -> List[Position]:
+    pass
